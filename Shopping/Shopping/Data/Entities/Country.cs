3cf23538969b1d6b29d58c5982747c3f838e7bfb
Mapi1537 <106278026+Mapi1537@users.noreply.github.com>
2022-07-01 17:40:57 -0500

    creating connection with DB

---
+++ Shopping/Shopping/Data/Entities/Country.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Shopping.Data.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        
+        [Display(Name = "Pais")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener {1} cracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public int Name { get; set; }
+
+    }
+}
